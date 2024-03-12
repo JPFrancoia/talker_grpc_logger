d08962a735ada39339d6c770678dea3ce57d74a3
JPFrancoia <jeanpatrick.francoia@gmail.com>
2024-03-12 20:40:20 +0000

    Using version 3 of talker for compatibility reasons.

--- lib/talker_grpc_logger.dart
+++ lib/talker_grpc_logger.dart
@@ -1,8 +1,3 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library;
+library talker_grpc_logger;
 
 export 'src/talker_grpc_logger_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
