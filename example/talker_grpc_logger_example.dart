1e188c83516ccdbadcef970da10d17a80095d4dc
JPFrancoia <jeanpatrick.francoia@gmail.com>
2024-03-12 19:36:02 +0000

    Initial commit, just adding boilerplate.

---
+++ example/talker_grpc_logger_example.dart
@@ -0,0 +1,6 @@
+import 'package:talker_grpc_logger/talker_grpc_logger.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/src/talker_grpc_logger_base.dart
@@ -0,0 +1,31 @@
+/// Implements a gRPC interceptor that logs requests and responses to Talker.
+/// https://pub.dev/documentation/grpc/latest/grpc/ClientInterceptor-class.html
+
+import 'package:talker/talker.dart';
+import 'package:grpc/grpc.dart';
+
+class TalkerHttpLogger extends ClientInterceptor {
+  TalkerHttpLogger({Talker? talker}) {
+    _talker = talker ?? Talker();
+  }
+
+  late Talker _talker;
+
+  @override
+  ResponseFuture<R> interceptUnary<Q, R>(ClientMethod<Q, R> method, Q request,
+      CallOptions options, ClientUnaryInvoker<Q, R> invoker) {
+    print('interceptUnary');
+    return invoker(method, request, options);
+  }
+
+  @override
+  ResponseStream<R> interceptStreaming<Q, R>(
+      ClientMethod<Q, R> method,
+      Stream<Q> requests,
+      CallOptions options,
+      ClientStreamingInvoker<Q, R> invoker) {
+    print('interceptStreaming');
+
+    return invoker(method, requests, options);
+  }
+}
